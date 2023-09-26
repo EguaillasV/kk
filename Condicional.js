d595b61fc22e211d3f1fa774d0e16b7aaae32b06
EguaillasV <133285900+EguaillasV@users.noreply.github.com>
2023-09-26 09:20:59 -0500

    Add files via upload

---
+++ Condicional.js
@@ -0,0 +1,87 @@
+
+//if
+let conditional_if = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//swicth
+
+let day = 3;
+let message= ""
+
+switch(day){
+    case 1:
+        message = "lunes";
+        break;
+case 2 :
+    message= "martes";
+    break;
+case 3 :
+    message= "miercoles";
+    break;
+case 4 :
+    message= "jueves";
+    break;
+case 5 :
+    message= "viernes";
+    break;
+default:
+    message= "fin de semana";
+}
+
+
+
+
+
+//for
+
+for (let i = 0; i < 5; i++) {
+    console.log (i); 0,1,2,3,4
+    }
+
+    chain = "hola mundo";
+    for (let i = 0; i < chain.length; i++){
+        console.log (chain[i]);
+        }
+
+    chain = "hola mundo";
+    for (let i = 0; i < chain.length; i++){
+        if (chain [i] !== ``) {
+         console.log (chain[i]);
+            }
+            }
+
+            //while
+let counter = 0;
+
+while ( counter < 5){
+    console.log (counter);
+    counter++;
+    }
+
+let counter_ = '';
+let i = 0;
+
+do {
+  i = i + 1;
+  counter_ = counter_ + i;
+} while (i < 5);
+
+console.log(counter_);
+
+
+for (let i = 0; i < chain.length; i++){
+   chain[i] !== ` `? console.log (chain[i]):null;
+   }
+    
